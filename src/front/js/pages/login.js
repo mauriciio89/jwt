444df128db631be375b347a961e42d316b873ef6
mauriciio89 <maugalvez89@gmail.com>
2022-10-16 05:28:52 +0000

    pfr

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
+
+  const handleClick = () => {
+    const opts = {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+
+    fetch(
+      "https://4geeksacade-reactflaskh-jt0quklw2uq.ws-us71.gitpod.io/api//token",
+      opts
+        .then((resp) => {
+          if (resp.status === 200) return resp.json();
+          else alert("there has been some error");
+        })
+        .then()
+        .catch((error) => {
+          console.error("there was an error!!!", error);
+        })
+    );
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Login</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleClick}>Login</button>
+      </div>
+    </div>
+  );
+};
